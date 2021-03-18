09d37be80cea065377b7fdfcd36bcf5133ecc81d
RichardRNS <71282342+RichardRNS@users.noreply.github.com>
2021-03-18 14:02:56 -0300

    Add files via upload

---
+++ QuizScript.js
@@ -0,0 +1,81 @@
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const myQuestions = [
+  {
+    question: "1. The founder of Illuminati and his alias is",
+    answers: {
+      a: "Adam Wishaupt, Ajax",
+      b: "Adam Wishaupt, Spartacus",
+      c: "Massenhausen, Ajax",
+      d: "Bauhof, Agathon"
+    },
+    correctAnswer: "b"
+  },
+  {
+    question: "2. The goal of illuminati is",
+    answers: {
+      a: "Equity, freedom, self-rule",
+      b: "Darvinism, survival of the strongest",
+      c: "Monarchy, dictatorship, new world order",
+      d: "Contact aliens, nuke the world"
+    },
+    correctAnswer: "a"
+  },
+  {
+    question: "3. Regarding the French Revolution, the illuminati",
+    answers: {
+      a: "Was the mastermind",
+      b: "Didn't participate at all",
+      c: "Partly participated, but failed and some were executed later on.",
+      d: "Was on the side of King Louis and were executed with him"
+    },
+    correctAnswer: "c"
+  }
+];
+function buildQuiz(){
+    const output = [];
+    myQuestions.forEach(
+      (currentQuestion, questionNumber) => {
+        const answers = [];
+        for(letter in currentQuestion.answers){
+          answers.push(
+            `<label>
+              <input type="radio" name="question${questionNumber}" value="${letter}">
+              ${letter} :
+              ${currentQuestion.answers[letter]}
+            </label>`
+          );
+        }
+        output.push(
+          `<div class="question"> ${currentQuestion.question} </div>
+          <div class="answers"> ${answers.join('')} </div>`
+        );
+      }
+    );
+    quizContainer.innerHTML = output.join('');
+}
+
+function showResults(){
+    const answerContainers = quizContainer.querySelectorAll('.answers');
+    let numCorrect = 0;
+    myQuestions.forEach( (currentQuestion, questionNumber) => {
+      const answerContainer = answerContainers[questionNumber];
+      const selector = `input[name=question${questionNumber}]:checked`;
+      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+      if(userAnswer === currentQuestion.correctAnswer){
+        numCorrect++;
+        answerContainers[questionNumber].style.color = 'lightgreen';
+      }
+      else{
+        answerContainers[questionNumber].style.color = 'red';
+      }
+    });
+    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+  }
+
+
+buildQuiz();
+
+
+submitButton.addEventListener('click', showResults);
